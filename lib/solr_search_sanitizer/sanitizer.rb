2d20f44ba203ecd30b726014a170360dc61a158e
Minus <minustehbare@gmail.com>
2011-07-12 09:40:28 -0400

    finalizing escape/remove methods, making regexps less strict

--- lib/solr_search_sanitizer/sanitizer.rb
+++ lib/solr_search_sanitizer/sanitizer.rb
@@ -1,35 +1,39 @@
 module SolrSearchSanitizer
   module Sanitizer
-    BOOLEAN_STRING_OPERATOR_REGEXP = /\s(AND)\s|\s(OR)\s)|\s(NOT)\s/
-    BOOLEAN_SYMBOL_OPERATOR_REGEXP = /\s(&&)\s|\s(!)\s)|\s(\|\|)\s/
-    BRACKET_REGEXP = /((\(|\))|(\{|\})|(\[|\]))/
-    WILDCARD_REGEXP = /((\*|\?))/
-    FUZZY_REGEXP = /[\w|\"](~)[\d]?/
-    BOOST_REGEXP = /[\w|\"](\^)[\d]?/
-    BOOLEAN_MODIFIER_REGEXP = /(\+|-)[(\w+(\s|\b))|(\".+\")]/
-    MISC_REGEXP = //
+    BOOLEAN_STRING_OPERATOR_REGEXP = /\s(AND)\s|\s(OR)\s|\s(NOT)\s/
+    BOOLEAN_SYMBOL_OPERATOR_REGEXP = /\s(&&)\s|\s(!)\s|\s(\|\|)\s/
+    BRACKET_REGEXP = /((\()|(\))|(\{)|(\})|(\[)|(\]))/
+    WILDCARD_REGEXP = /((\*)|(\?))/
+    #FUZZY_REGEXP = /[\w|\"](~)[\d]?/
+    FUZZY_REGEXP = /(~)/
+    #BOOST_REGEXP = /[\w|\"](\^)[\d]?/
+    BOOST_REGEXP = /(\^)/
+    #BOOLEAN_MODIFIER_REGEXP = /((\+)|(-))[(\w+(\s|\b))|(\".+\")]/
+    BOOLEAN_MODIFIER_REGEXP = /((\+)|(-))/
+    MISC_REGEXP = /(\")|(:)/
     
     ####################
     #
     # All REGEXP definitions match characters or keywords that are part of the Apache Lucene Query Parser Sytntax
     #
     # BOOLEAN_OPERATOR_REGEXP => matches boolean operators on multiple search terms
-    # these operators should be removed from a basic query since they do not incorporate
     #
     # BRACKET_REGEXP => matches parentheses and brackets and braces that group search clauses or define range searches
     #
     # WILDCARD_REGEXP => matches ? or * characters in the search that act as single or multiple character wildcards within a search term
     #
-    # FUZZY_REGEXP => matches the ~ character at the end of a search term or phrase 
+    # FUZZY_REGEXP => matches the ~ character at the end of a search term or phrase
     #
     # BOOST_REGEXP => matches the ^ character at the end of a search term or phrase
     #
     # BOOLEAN_MODIFIER_REGEXP => matches + or - characters at the start of search terms/phrases
     #
+    # MISC_REGEXp => matches (currently) : and " characters that are used for defining phrases or field values
+    #
     ####################
     
     def escape_boolean_operators(query)
-      new_query = new_query.gsub(BOOLEAN_SYMBOL_OPERATOR_REGEXP, '\\\\\1')
+      new_query = query.gsub(BOOLEAN_SYMBOL_OPERATOR_REGEXP, ' && ' => ' \\&\\& ', ' || ' => ' \\|\\| ', ' ! ' => ' \\! ')
     end
     
     def remove_boolean_operators(query)
@@ -37,39 +41,51 @@ module SolrSearchSanitizer
     end
     
     def escape_brackets(query)
+      new_query = query.gsub(BRACKET_REGEXP, '\\\\\1'
     end
     
     def remove_brackets(query)
+      new_query = query.gsub(BRACKET_REGEXP, '')
     end
     
     def escape_wildcards(query)
+      new_query = query.gsub(BRACKET_REGEXP, '\\\\\1')
     end
     
     def remove_wildcards(query)
+      new_query = query.gsub(WILDCARD_REGEXP, '')
     end
     
     def escape_fuzzy(query)
+      new_query = query.gsub(BRACKET_REGEXP, '\\\\\1')
     end
     
     def remove_fuzzy(query)
+      new_query = query.gsub(FUZZY_REGEXP, '')
     end
     
     def escape_boost(query)
+      new_query = query.gsub(BRACKET_REGEXP, '\\\\\1')
     end
     
     def remove_boost(query)
+      new_query = query.gsub(BOOST_REGEXP, '')
     end
     
     def escape_boolean_modifiers(query)
+      new_query = query.gsub(BOOLEAN_MODIFIER_REGEXP, '\\\\\1')
     end
     
     def remove_boolean_modifiers(query)
+      new_query = query.gsub(BOOLEAN_MODIFIER_REGEXP, '')
     end
-    def remove_boolean_operators(query)
-      query.gsub(BOOLEAN_OPERATOR_REGEXP, '')
+    
+    def escape_misc(query)
+      new_query = query.gsub(MISC_REGEXP, '"' => '\\"', ':', '\\:')
     end
-    def escape_special_characters(query)
-      query.gsub(BRACKEY_REGEXP.union(WILDCARD_REGEXP).union(FUZZY_REGEXP).union(BOOST_REGEXP).union(BOOLEAN_MODIFIER_REGEXP), '\\\\\1'
+    
+    def remove_misc(query)
+      new_query = query.gsub(MISC_REGEXP, '')
     end
   end
 end
